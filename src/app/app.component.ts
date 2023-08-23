4e72f28c29401be5e23ef06fe4b15f5fa3616459
Aqeel Abbas <aqeelabbas11221122@gmail.com>
2023-08-23 13:51:35 +0500

    reactive-forms-validation

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'angular-project-youtube';
+
+  formGroup!: FormGroup;
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  saveData(data: any) {
+    console.log(data);
+  }
+
+  get name() {
+    return this.formGroup.get('name');
+  }
+  get email() {
+    return this.formGroup.get('email');
+  }
+  get password() {
+    return this.formGroup.get('password');
+  }
+}
