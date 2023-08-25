ad51812d183fb0bd0f08326c6e581066b7b729a0
Aqeel Abbas <aqeelabbas11221122@gmail.com>
2023-08-25 12:37:48 +0500

    add Routing module in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, DynamicFormComponent],
+  declarations: [
+    AppComponent,
+    DynamicFormComponent,
+    HomeComponent,
+    AboutComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
